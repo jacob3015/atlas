b7a0a99faf768c39b086a114a862b23958f5db3f
Jaimin Pak <jacob3015@gmail.com>
2026-06-28 23:14:33 +0900

    refactor: move hello command to example command group

--- src/atlas/cli/hello.py
+++
@@ -1,8 +0,0 @@
-import typer
-
-
-def register(app: typer.Typer) -> None:
-    @app.command()
-    def hello(name: str) -> None:
-        """Print a greeting."""
-        typer.echo(f"Hello, {name}!")
